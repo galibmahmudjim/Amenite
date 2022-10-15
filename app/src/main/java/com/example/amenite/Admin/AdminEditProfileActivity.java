087c09baffb75115e30c97d9895f75cf233713a7
Lubna-SK25 <85923460+Lubna-SK25@users.noreply.github.com>
2022-10-15 17:07:36 +0600

    admin UI(admin home,admin edit profile & profile)& images

---
+++ app/src/main/java/com/example/amenite/Admin/AdminEditProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.amenite.Admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminEditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_edit_profile);
+    }
+}
