377c3b198f3907924691e5d7157c58d36ff5c745
galibmahmudjim <galibmahmudjim@gmail.com>
2022-11-04 21:55:10 +0600

    Task Complete and rating

--- app/src/main/java/com/example/amenite/Employee/EmployeeHomeFragment.java
+++ app/src/main/java/com/example/amenite/Employee/EmployeeHomeFragment.java
@@ -2,26 +2,40 @@ package com.example.amenite.Employee;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.example.amenite.Customer.Appointmentlist.AppointmentList;
+import com.example.amenite.DBRes.DBresources;
 import com.example.amenite.PROFILE.User;
 import com.example.amenite.R;
+import com.example.amenite.TAG;
 import com.example.amenite.databinding.FragmentEmployeeHomeBinding;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class EmployeeHomeFragment extends Fragment {
 
     private FragmentEmployeeHomeBinding binding;
-
+    SwipeRefreshLayout mSwipeRefreshLayout;
+    ArrayList<AppointmentList> appointmentLists;
     public EmployeeHomeFragment() {
     }
 
@@ -29,7 +43,74 @@ public class EmployeeHomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentEmployeeHomeBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
+        appointmentLists = new ArrayList<>();
+         mSwipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.pullToRefresh);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mSwipeRefreshLayout.setRefreshing(false);
+            }
+        });
+        mSwipeRefreshLayout.setRefreshing(true);
+        homeset();
+
+        binding.employeehomecurrentappointmentcompletebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(),TaskCompleteActivity.class);
+                DBresources dBresources = new DBresources();
+                dBresources.database.collection("Appointment").document(appointmentLists.get(1).getAppointment_Id())
+                                .get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                            @Override
+                            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                intent.putExtra("Email",documentSnapshot.get("Email").toString());
+                                startActivity(intent);
+                            }
+                        });
+            }
+        });
         TextView toolbartextview = getActivity().findViewById(R.id.EmployeeActivityToolbarTextview);
         return view;
     }
+    void homeset()
+    {
+        DBresources dBresources = new DBresources();
+        appointmentLists = new ArrayList<>();
+        Task t1 = dBresources.database.collection("Appointment").whereEqualTo("Employee",User.Emailid)
+                .get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        for(QueryDocumentSnapshot queryDocumentSnapshot : queryDocumentSnapshots)
+                        {
+                            if(queryDocumentSnapshot.get("Status").equals("Accepted"))
+                            {
+                                appointmentLists.add(queryDocumentSnapshot.toObject(AppointmentList.class));
+                            }
+                        }
+                        appointmentLists.stream()
+                                .sorted(Comparator.comparing(AppointmentList::getAppointment_Date))
+                                .collect(Collectors.toList());
+                    }
+                });
+        Tasks.whenAllSuccess(t1).addOnSuccessListener(new OnSuccessListener<List<Object>>() {
+            @Override
+            public void onSuccess(List<Object> objects) {
+                mSwipeRefreshLayout.setRefreshing(false);
+                binding.employeehomecurrentappointment.EmployeeAppointmentlistCardviewTimeTextview.setText(appointmentLists.get(1).getRequest_Date()+", "+appointmentLists.get(1).getAppointment_Time());
+                binding.employeehomecurrentappointment.EmployeeAppointmentlistCardviewIdTextview.setText(appointmentLists.get(1).getAppointment_Id());
+                binding.employeehomecurrentappointment.EmployeeAppointmentlistCardviewServiceTextview.setText(appointmentLists.get(1).getService());
+                binding.employeehomecurrentappointment.EmployeeAppointmentlistRating.setVisibility(View.GONE);
+                binding.employeehomecurrentappointment.EmployeeAppointmentlistCardviewAppointmentTimeTextview.setText(appointmentLists.get(1).getAppointment_Time());
+                binding.employeehomecurrentappointment.EmployeeAppointmentlistCardviewAppointmentDateTextview.setText(appointmentLists.get(1).getAppointment_Date());
+                binding.employeehomenextappointment.EmployeeAppointmentlistCardviewTimeTextview.setText(appointmentLists.get(1).getRequest_Date()+", "+appointmentLists.get(1).getAppointment_Time());
+                binding.employeehomenextappointment.EmployeeAppointmentlistCardviewIdTextview.setText(appointmentLists.get(2).getAppointment_Id());
+                binding.employeehomenextappointment.EmployeeAppointmentlistCardviewServiceTextview.setText(appointmentLists.get(2).getService());
+                binding.employeehomenextappointment.EmployeeAppointmentlistRating.setVisibility(View.GONE);
+                binding.employeehomenextappointment.EmployeeAppointmentlistCardviewAppointmentTimeTextview.setText(appointmentLists.get(2).getAppointment_Time());
+                binding.employeehomenextappointment.EmployeeAppointmentlistCardviewAppointmentDateTextview.setText(appointmentLists.get(2).getAppointment_Date());
+
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/amenite/Employee/TaskCompleteActivity.java
+++ app/src/main/java/com/example/amenite/Employee/TaskCompleteActivity.java
@@ -1,21 +1,62 @@
 package com.example.amenite.Employee;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.RatingBar;
 
+import com.bumptech.glide.Glide;
+import com.example.amenite.Customer.Appointmentlist.AppointmentList;
+import com.example.amenite.DBRes.DBresources;
+import com.example.amenite.PROFILE.User;
 import com.example.amenite.R;
 import com.example.amenite.databinding.ActivityTaskCompleteBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class TaskCompleteActivity extends AppCompatActivity {
     private ActivityTaskCompleteBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityTaskCompleteBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         Intent intent = getIntent();
+        final double[] rating = {0.0};
+        binding.taskcompleterating.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                rating[0] = v;
+            }
+        });
+        AppointmentList appointmentList = getIntent().getParcelableExtra("Object");
+        DBresources dBresources = new DBresources();
+        User.RetriveData().addOnSuccessListener(new OnSuccessListener() {
+            @Override
+            public void onSuccess(Object o) {
+                dBresources.database.collection("User").whereEqualTo("Email", getIntent().getStringExtra("Email"))
+                        .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                for (QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()) {
+                                    binding.taskcompletename.setText(queryDocumentSnapshot.get("Name").toString());
+                                    if (queryDocumentSnapshot.contains("Profile_Pic")) {
+                                        Glide.with(TaskCompleteActivity.this)
+                                                .load(queryDocumentSnapshot.get("Profile_Pic"))
+                                                .into(binding.taskcompleteprofilepc);
+                                    }
+                                }
+                            }
+                        });
+            }
+        });
+
 
     }
 }
