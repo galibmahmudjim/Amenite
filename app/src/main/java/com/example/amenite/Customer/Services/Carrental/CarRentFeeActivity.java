85ad4171d30b529a8311bdae012ee4eb0fd59253
galibmahmudjim <galibmahmudjim@gmail.com>
2022-11-04 15:02:25 +0600

    Action bar and car rental confirm activity

--- app/src/main/java/com/example/amenite/Customer/Services/Carrental/CarRentFeeActivity.java
+++ app/src/main/java/com/example/amenite/Customer/Services/Carrental/CarRentFeeActivity.java
@@ -3,6 +3,7 @@ package com.example.amenite.Customer.Services.Carrental;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -16,6 +17,8 @@ public class CarRentFeeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityCarRentFeeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        Intent intent = new Intent(CarRentFeeActivity.this,CarRentalConfirmActivity.class);
+        intent.putExtras(getIntent().getExtras());
         binding.cartoolbar.appbartitle.setText("Car Rental");
         if(getIntent().getStringExtra("Service_Area").equals("Outside City"))
         {
@@ -32,18 +35,25 @@ public class CarRentFeeActivity extends AppCompatActivity {
         binding.budgetcardview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                    intent.putExtra("Car_Catagory",binding.carcat1.getText());
+                    intent.putExtra("Payment",binding.carfee1.getText());
+                    startActivity(intent);
             }
         });
         binding.familycardview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                intent.putExtra("Car_Catagory",binding.carcat2.getText());
+                intent.putExtra("Payment",binding.carfee2.getText());
+                startActivity(intent);
             }
         });
         binding.groupcardview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                intent.putExtra("Car_Catagory",binding.carcat3.getText());
+                intent.putExtra("Payment",binding.carfee3.getText());
+                startActivity(intent);
             }
         });
 
--- app/src/main/java/com/example/amenite/Customer/Services/Carrental/CarRentalConfirmActivity.java
+++ app/src/main/java/com/example/amenite/Customer/Services/Carrental/CarRentalConfirmActivity.java
@@ -1,16 +1,51 @@
 package com.example.amenite.Customer.Services.Carrental;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.amenite.R;
+import com.example.amenite.databinding.ActivityCarRentalConfirmBinding;
 
 public class CarRentalConfirmActivity extends AppCompatActivity {
-
+    private ActivityCarRentalConfirmBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_car_rental_confirm);
+        binding = ActivityCarRentalConfirmBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.carconfirmtoolbar.appbartitle.setText("Car Rental");
+        binding.carconfirmtoolbar.toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+        binding.carfeecon.setText(getIntent().getStringExtra("Payment"));
+        binding.carcatcon.setText(getIntent().getStringExtra("Car_Catagory"));
+        if(getIntent().getStringExtra("Car_Catagory").equals("Premium"))
+        {
+            binding.carpic.setImageDrawable(ResourcesCompat.getDrawable(getResources(),R.drawable.carpic1,null));
+            binding.carconcapacity.setText("4 seats");
+        }
+        else if(getIntent().getStringExtra("Car_Catagory").equals("Family"))
+        {
+            binding.carpic.setImageDrawable(ResourcesCompat.getDrawable(getResources(),R.drawable.carpic2,null));
+            binding.carconcapacity.setText("1-7 seats");
+        }
+        else if(getIntent().getStringExtra("Car_Catagory").equals("Group"))
+        {
+            binding.carpic.setImageDrawable(ResourcesCompat.getDrawable(getResources(),R.drawable.group,null));
+            binding.carconcapacity.setText("1-12 seats");
+        }
+        if(getIntent().getStringExtra("Service_Area").equals("Inside City(Dhaka)"))
+            binding.carconfirmtextview.setText(getIntent().getStringExtra("Time_Limit")+"-"+getIntent().getStringExtra("Service_Area")+"-"+"Car Rental");
+        else
+            binding.carconfirmtextview.setText(getIntent().getStringExtra("Service_Area")+"-"+"Car Rental");
+
+
     }
 }
